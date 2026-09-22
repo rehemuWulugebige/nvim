2629c93a6f1503b35d36dfed8b2ca6a4823e68a6
Rehemu Wulugebige <rehemu.wulugebige@gmail.com>
2026-09-21 23:55:28 -0400

    lua_ls: index only VIMRUNTIME

--- lua/me/lazy/lsp.lua
+++ lua/me/lazy/lsp.lua
@@ -36,7 +36,7 @@ return {
 						runtime = { version = "LuaJIT" },
 						diagnostics = { globals = { "vim" } },
 						workspace = {
-							library = vim.api.nvim_get_runtime_file("", true),
+                            library = { vim.env.VIMRUNTIME },
 							checkThirdParty = false,
 						},
 					},
--- lua/me/set.lua
+++ lua/me/set.lua
@@ -1,3 +1,7 @@
+vim.opt.guicursor = "" -- block cursor in every mode
+
+vim.opt.nu = true
+vim.opt.relativenumber = true -- makes 12j / 8k jumps trivial
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
